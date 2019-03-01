f5323b51b39aec40863c02c3bdb7c2eff2272412
namadi <nmuawiyah@gmail.com>
2019-03-01 11:26:23 +0100

    lecture view refactor

--- atktut/questions/views.py
+++ atktut/questions/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import Answer, Question
 from .serializers import AnswerSerializer, QuestionSerializer
 
-from atktut.course.models import Progress, Lesson
+from atktut.course.models import Progress, Lesson, Unit
 from atktut.course.serializers import LessonSerializer
 
 
@@ -31,45 +31,37 @@ class AnswerViewSet(viewsets.ModelViewSet):
 @api_view(['POST'])
 @permission_classes((permissions.IsAuthenticated, ))
 def progress(request):
-    unit = request.data.get('unit')
-    course = request.data.get('course')
-    lesson_id = request.data.get('lesson')
-    user = request.user
-    progress = None
-    next_lesson = None
-    query = Lesson.objects.filter(unit_id=unit).order_by('order')
-    total_lessons = query.count()
+    unit_id = request.data.get('unit_id')
+    course_id = request.data.get('course_id')
+    lesson_id = request.data.get('lesson_id')
 
-    if not unit or not course or not lesson_id:
+    if not unit_id or not course_id or not lesson_id:
         return Response(data={'message': 'invalid request'},
                         status=status.HTTP_400_BAD_REQUEST)
-
-    current_index = list(query.values_list('id', flat=True)).index(int(lesson_id))
-    lesson = query[current_index]
-    try:
-        next_index = list(query.values_list('order', flat=True)).index(int(lesson.order) + 1)
-        next_lesson = query[next_index]
-    except Exception:
-        pass
+    
+    progress = None
+    query = Lesson.objects.filter(unit_id=unit_id).order_by('order')
+    next_lesson = get_next_lesson(request.user, course_id, unit_id, lesson_id, query)
 
     try:
-        progress = Progress.objects.get(owner=user, course_id=course)
+        progress = Progress.objects.get(owner=request.user, course_id=course_id)
     except Exception:
         pass
 
     if not progress:
         progress = Progress.objects.create(
-            total_lessons=total_lessons,
-            unit_id=unit,
-            lesson_id=lesson.id,
-            owner=user,
-            course_id=course,
+            total_lessons=query.count(),
+            unit_id=unit_id,
+            lesson_id=lesson_id,
+            owner=request.user,
+            course_id=course_id,
         )
-        progress.completed_lessons.add(lesson)
+        progress.completed_lessons.add(lesson_id)
     else:
-        progress.lesson = lesson
-        progress.total_lessons = total_lessons
-        progress.unit_id = unit
+        lesson = query[list(query.values_list('id', flat=True)).index(int(lesson_id))]
+        progress.lesson_id = lesson_id
+        progress.total_lessons = query.count()
+        progress.unit_id = unit_id
         progress.save
         if lesson not in progress.completed_lessons.all():
             progress.completed_lessons.add(lesson)
@@ -80,6 +72,7 @@ def progress(request):
 @api_view(['POST'])
 @permission_classes((permissions.IsAuthenticated, ))
 def answer(request):
+    course_id = request.data.get('course_id')
     question_id = request.data.get('question_id')
     answers = request.data.get('answer')
     unit_id = request.data.get('unit_id')
@@ -104,14 +97,54 @@ def answer(request):
             correct = False
 
     if correct:
-        next_lesson = None
-        query = Lesson.objects.filter(unit_id=unit_id).order_by('order')
-        lesson = query[list(query.values_list('id', flat=True)).index(int(lesson_id))]
-        try:
-            next_lesson = query[list(query.values_list('order', flat=True)).index(int(lesson.order) + 1)]
-        except Exception:
-            pass
+        next_lesson = get_next_lesson(request.user, course_id, unit_id, lesson_id)
         serializer = LessonSerializer(next_lesson)
         return Response(serializer.data)
 
     return Response(data={'message': 'answer incorrect'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_next_lesson(user, course_id, unit_id, lesson_id, query=None):
+
+    if not query:
+        query = Lesson.objects.filter(unit_id=unit_id).order_by('order')
+    lesson = query[list(query.values_list('id', flat=True)).index(int(lesson_id))]
+
+    progress = None
+    try:
+        progress = Progress.objects.get(owner=user, course_id=course_id)
+    except Exception:
+        pass
+
+    if not progress:
+        progress = Progress.objects.create(
+            total_lessons=query.count(),
+            unit_id=unit_id,
+            lesson_id=lesson_id,
+            owner=user,
+            course_id=course_id,
+        )
+        progress.completed_lessons.add(lesson_id)
+    else:
+        if lesson not in progress.completed_lessons.all():
+            progress.lesson_id = lesson_id
+            progress.total_lessons = query.count()
+            progress.unit_id = unit_id
+            progress.save
+            progress.completed_lessons.add(lesson)
+    try:
+        next_lesson = query[list(query.values_list('order', flat=True)).index(int(lesson.order) + 1)]
+        if not next_lesson:
+            unit_query = Unit.objects.filter(course_id=course_id).order_by('order')
+            unit = unit_query[list(unit_query.values_list('id', flat=True)).index(int(unit_id))]
+            next_unit = unit_query[list(unit_query.values_list('order', flat=True)).index(int(unit.order) + 1)]
+            
+            if next_unit:
+                query = Lesson.objects.filter(unit_id=next_unit.id).order_by('order')
+                return query[0]
+            else:
+                return None
+        else:
+            return next_lesson
+    except Exception:
+        return None
