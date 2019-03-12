8ade8b4e9206989f2a369911548f0bc38aaf86ef
namadi <nmuawiyah@gmail.com>
2019-03-12 16:24:56 +0100

    user factory test fixes

--- atktut/users/test/factories.py
+++ atktut/users/test/factories.py
@@ -12,6 +12,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     password = factory.Faker('password', length=10, special_chars=True, digits=True,
                              upper_case=True, lower_case=True)
     email = factory.Faker('email')
+    headshot = factory.django.ImageField(color='blue')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     is_active = True
